9a86976e1fe0ad6c4da448098c39a91a48934519
caseyokane <casey.okane@uky.edu>
2016-08-11 21:00:05 -0400

    initial design documentation included

--- Proj1TempSensor/raspbPiTempSensor.cpp
+++ Proj1TempSensor/raspbPiTempSensor.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
-int main(){
+
+class tempInfo{
+	public:
+		int tempOutinC;
+}; 
+
+/*
+ * Method that connects with the HTS221 and retreives the temperature based
+ * information
+ *
+ *
+ */
+int initDeviceComms(){
+    //Initialize file descriptor, buffer and I2C address
+    //Open the bus to communicate with ADC device
+    //Use ioctl to talk to the device
+    //Read the information from the device using i2c
+    //Return temperature related information
+    return 0; 
+} 
+
+
+int main(int argc, char* argv[]){
 	std::cout<<"In progress...\n";
+    //Use while loop to constantly receive the temperature and print it
+    //Call initDeviceComms to get the associated temperature
 	return 0;
 }
+
+
+//TODO: Decide how we want to present information.... 
+    /*Constant while loop that continues to print out temperature (inefficient?)
+     *Call device communication function when button is pressed
+    */
