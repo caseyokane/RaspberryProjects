8558bf7e3bae34b26e2ccc510d8f8f5662450ac8
caseyokane <casey.okane@uky.edu>
2016-08-18 04:43:43 +0000

    Making additional changes. Still need to confirm proper device address

--- Proj1TempSensor/raspbPiTempSensor.cpp
+++ Proj1TempSensor/raspbPiTempSensor.cpp
@@ -8,12 +8,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+const int slaveAddr = 0xbf; 
+const int HTS221whoami = 0xbc;
+const char * i2cLoc = "/dev/i2c-1";
+
 class tempInfo{
 	public:
 		int tempOutinC;
 }; 
-
-
 
 /*
  * Method that connects with the HTS221 and retreives the temperature based
@@ -24,30 +26,33 @@ class tempInfo{
 int initDeviceComms(){
     //Initialize file descriptor, buffer and I2C address
     int fd = 0;
-    char* fileName = NULL;
     char buffer[42] = {};
-    int devAddr = 0xbf;
-    
+ 
     //Open the bus to communicate with ADC device
-    sprintf(fileName, "/dev/i2c-2");
-    if(( fd = open(fileName, O_RDWR)) < 0){
+    if(( fd = open(i2cLoc, O_RDWR)) < 0){
         std::cout << "Issue opening the device bus...\n";
         exit(1);
     }
-    
+
     //Use ioctl to talk to the device
-    if(ioctl(fd, I2C_SLAVE, devAddr) < 0) {
+    if(ioctl(fd, I2C_SLAVE, slaveAddr) < 0) {
         std::cout << "Issue communicating with the device...\n";
         exit(1);
     }    
 
+	std::cout << "assigning buffer to whoami" << std::endl;	
+	
+    buffer[0] = HTS221whoami;
+
+	std::cout << "reading information from whoami" << std::endl;
     //Read the information from the device using i2c one byte at a time
     if(read(fd, buffer, 1) <0){
         std::cout << "Issue reading from device bus...\n";
         exit(1);
     }
 
-    //Return temperature related information
+    std::cout << "Returned: " << buffer[0] << std::endl;
+
     return buffer[0]; 
 } 
 
@@ -56,11 +61,11 @@ int initDeviceComms(){
  * received
  */
 int main(int argc, char* argv[]){
-	std::cout<<"In progress...\n";
+	std::cout<<"In progress..."<<std::endl;
 
 	tempInfo temperatureObj;
-    	//Use while loop to constantly receive the temperature and print it
-    	//Call initDeviceComms to get the associated temperature
+	temperatureObj.tempOutinC = initDeviceComms();
+	std::cout<<"Current Temperature (in Celsius): "<<temperatureObj.tempOutinC<<std::endl;
 	return 0;
 }
 
